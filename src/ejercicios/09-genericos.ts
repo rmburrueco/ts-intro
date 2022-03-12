8bc8edc5bdda3d34e8256dd9eb720b7a627e323c
Rosa M. Burrueco <rosam.burrueco@juntadeandalucia.es>
2022-03-12 13:07:15 +0100

    Genéricos

---
+++ src/ejercicios/09-genericos.ts
@@ -0,0 +1,14 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+function queTipoSoy<T>(argumento: T) {
+    return argumento;
+}
+
+let soyString = queTipoSoy('Hola Mundo');
+let soyNumero = queTipoSoy(100);
+let soyArray = queTipoSoy([1,2,3,4,5]);
+
+let soyExplicito = queTipoSoy<number>( 100 );
+
